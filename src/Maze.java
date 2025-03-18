b60cf5a5d0ef1d2c5e74f9fd468c3294e1a11b34
rayyanr08 <rayyanrahman26@gmail.com>
2025-03-17 20:43:06 -0400

    Update Maze.java
    finished part 2

--- src/Maze.java
+++ src/Maze.java
@@ -57,34 +57,6 @@ public class Maze {
     return false;
     }
 
-    public boolean ifMultipleDirections(){
-        int count = 0;
-        if (ifDown()) count++;
-        if (ifUp()) count++;
-        if (ifLeft()) count++;
-        if (ifRight()) count++;
-        if (count > 1){
-            return true;
-        }
-        return false;
-    }
-    public void addFork(){
-        Integer[] n = new Integer[]{xCoord,yCoord};
-        ifFork.add(n);
-    }
-
-    public boolean cantMove(){
-        int count = 0;
-        if (ifDown()) count++;
-        if (ifUp()) count++;
-        if (ifLeft()) count++;
-        if (ifRight()) count++;
-        if (count == 0) {
-            return true;
-        }
-        return false;
-    }
-
 
     public void part1(){
         ArrayList<String> solutions = new ArrayList<String>();
@@ -112,35 +84,58 @@ public class Maze {
         }
     }
 
-    public void part2(){
-        ArrayList<String> solutions = new ArrayList<String>();
-        solutions.add("(" + 0 + "," + 0 + ")");
-        while (xCoord != inputMaze.length-1 || yCoord != inputMaze[0].length-1) {
-            if (ifLeft()) {
-                solutions.add("(" + xCoord + "," + yCoord + ")");
-            }
-            if (ifRight()) {
-                solutions.add("(" + xCoord + "," + yCoord + ")");
-            }
-            if (ifUp()) {
-                solutions.add("(" + xCoord + "," + yCoord + ")");
-            }
-            if (ifDown()) {
-                solutions.add("(" + xCoord + "," + yCoord + ")");
-            }
-            if (cantMove()){
-                inputMaze[xCoord][yCoord] = "#";
-                xCoord = 0;
-                yCoord = 0;
+        public void part2() {
+        String[][] visited = new String[inputMaze.length][inputMaze[0].length];
+        
+        for (int i = 0; i < visited.length; i++) {
+            for (int j = 0; j < visited[0].length; j++) {
+                visited[i][j] = ".";
             }
         }
-        for (int i = 0; i<solutions.size();i++){
-            if (i != solutions.size()-1){
-                System.out.print(solutions.get(i) + "--->");
+
+        ArrayList<String> solutions = new ArrayList<>();
+        solutions.add("(" + xCoord + "," + yCoord + ")");
+        visited[xCoord][yCoord] = "+";
+
+        while (xCoord != inputMaze.length - 1 || yCoord != inputMaze[0].length - 1) {
+            boolean ifOpenSpace = false;
+
+            if (ifLeft() && visited[xCoord][yCoord].equals(".")) {
+                solutions.add("(" + xCoord + "," + yCoord + ")");
+                visited[xCoord][yCoord] = "+";
+                ifOpenSpace = true;
+            } else if (ifRight() && visited[xCoord][yCoord].equals(".")) {
+                solutions.add("(" + xCoord + "," + yCoord + ")");
+                visited[xCoord][yCoord] = "+";
+                ifOpenSpace = true;
+            } else if (ifUp() && visited[xCoord][yCoord].equals(".")) {
+                solutions.add("(" + xCoord + "," + yCoord + ")");
+                visited[xCoord][yCoord] = "+";
+                ifOpenSpace = true;
+            } else if (ifDown() && visited[xCoord][yCoord].equals(".")) {
+                solutions.add("(" + xCoord + "," + yCoord + ")");
+                visited[xCoord][yCoord] = "+";
+                ifOpenSpace = true;
+            }
+
+            if (!ifOpenSpace) {
+                inputMaze[xCoord][yCoord] = "#";
+                visited[xCoord][yCoord] = "#";
+
+                if (!solutions.isEmpty()) {
+                    solutions.remove(solutions.size() - 1);
+                }
+                xCoord = Integer.parseInt(solutions.get(solutions.size()-1).substring(1,2));
+                yCoord = Integer.parseInt(solutions.get(solutions.size()-1).substring(3,4));
+            }
+        }
+
+        for (int i = 0; i < solutions.size(); i++) {
+            if (i != solutions.size() - 1) {
+                System.out.print(solutions.get(i) + " ---> ");
             } else {
                 System.out.println(solutions.get(i));
             }
         }
-
     }
 }
